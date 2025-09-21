fa5f4b12a0a4d773bd516a543ee8924e94c55b3d
sharafath11 <abisharfath@gmail.com>
2025-09-21 20:54:15 +0530

    build frontend 1

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,9 +1,8 @@
 import path from "path"
-import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
+import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from "vite"
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
